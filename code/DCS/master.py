f1b64f4d6051704d56f9d42cfce64ed3667c566d
Charles-Code4Fun <67988493+Charles-Code4Fun@users.noreply.github.com>
2024-11-27 13:00:29 +1100

    Update master.py

--- code/DCS/master.py
+++ code/DCS/master.py
@@ -5,9 +5,13 @@ import time
 
 def main():
     # Dispatching tasks to the worker
-    result1 = square.apply_async((10,))  # Sending task to square the number 10
-    result2 = add.apply_async((5, 7))    # Sending task to add 5 and 7
+    # result1 = square.apply_async((10,))  # Sending task to square the number 10
+    # result2 = add.apply_async((5, 7))    # Sending task to add 5 and 7
 
+    # Assign the task to specific queues
+    result1 = square.apply_async((10,), queue='square_queue')
+    result2 = add.apply_async((5, 7), queue='add_queue')
+    
     # Wait for the results
     print(f"Task 1 result: {result1.get(timeout=10)}")
     print(f"Task 2 result: {result2.get(timeout=10)}")
